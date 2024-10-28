24b0c921bf5977d44e5d978fba1d299e582cd5fa
EvanGniadek <evan@gniadek.net>
2024-10-28 17:37:52 +0000

    Current code

--- MyLLTester.java
+++ MyLLTester.java
@@ -1,9 +1,27 @@
-public class MyLLTester.java
+public class MyLLTester
 {
   public static void main(String [] args)
   {
-    
+    MyLinkedList testList = new MyLinkedList(); 
+    testList.add("Tamerlane");
+    testList.addLast("Malinowski");
+    testList.addFirst("Napoleon");
+    for(int i = 0; i < 7; i++ )
+        testList.add("Evil horse" + i);
+    System.out.println("the contents of the list are" + testList.toString()); 
+    System.out.println("The current size is " + testList.size()); 
+    testList.set(5, "Luffy"); System.out.println("The sixth element is now " + testList.get(5) );
+    System.out.println("The first element is" + testList.get(0));
+    testList.removeFirst(); System.out.println("The first element was removed");
+    System.out.println(testList.toString());
+    testList.remove(6); System.out.println("The seventh element was removed");
+    System.out.println(testList.toString()); 
+    testList.removeLast(); System.out.println("The last element was removed");
+    System.out.println(testList.toString()); 
 
+
+    System.out.println("Is the list empty? " + testList.isEmpty());
+    System.out.println("How long is this LinkedList? "  + testList.size() + " Elements");
   } // end main
   
 } // end class
--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,5 +1,128 @@
 public class MyLinkedList
 {
+    private ListNode head;
+    
+    public MyLinkedList()
+    {
+        head = null;
+    }
 
+    public int size()
+    {
+        ListNode temp = head;
+        int count = 0;
+        while (temp != null)
+        {
+	        temp = temp.getNext();
+            count++;
+        }
+        return count;
+    }
 
-}
+    public boolean isEmpty()
+    {
+        if(head != null)
+            return false;
+        return true;
+    } //end of isempty
+
+    public boolean add(Object ob)
+    {
+        if(head == null){
+            head = new ListNode(ob, null);
+            return true;
+        }
+        ListNode temp = head;
+        while (temp.getNext() != null)
+        {
+	        temp = temp.getNext();
+        }
+        ListNode here =  new ListNode(ob, null);
+        temp.setNext(here);
+        return true;
+    } //end of add method 
+
+    public boolean addFirst(Object first)
+    {
+        ListNode here = new ListNode(first, head);
+        head = here;
+        return true;
+    }
+
+    public boolean addLast(Object ob)
+    {
+        return this.add(ob);
+    }
+
+    public Object set(int i, Object ob)
+    {
+        ListNode temp = head;
+        int j = 0;
+        while( j != i -1  && temp.getNext() != null)
+        {
+	        temp = temp.getNext();            
+        }//end of while 
+        temp.setValue(ob);
+        return ob;
+    }
+    
+    public Object get(int i)
+    {
+        ListNode temp = head;
+        int j = 0;
+        while( j != i - 1 && temp.getNext() != null)
+        {
+	        temp = temp.getNext();            
+        }//end of while 
+        return temp.getValue();
+        
+    }
+
+    public Object remove(int i)
+    {
+        ListNode temp = head;
+        int j = 0;
+        while( j < i - 2 && temp.getNext() != null)
+        {
+	        temp = temp.getNext();        
+            j++;
+        }//end of while 
+        Object value = temp.getNext().getValue();
+        temp.setNext(temp.getNext().getNext());
+        return value;
+    }
+
+    public Object removeLast()
+    {
+        ListNode temp = head;
+        while (temp.getNext().getNext() != null)
+        {
+	        temp = temp.getNext();
+        }
+        ListNode value = new ListNode(temp.getNext().getValue(), null);
+        temp.setNext(null);
+        return value;
+    }
+
+    public Object removeFirst()
+    {
+        Object temp = head.getValue();
+        head = head.getNext();
+        return temp;
+
+    }
+
+    public String toString(){
+        ListNode temp = head;
+        int count = 1;
+        String willBe = "";
+        while (temp != null)
+        {
+	        willBe = willBe + count + ": " + temp.getValue().toString() + " ";
+            temp = temp.getNext();
+            count++;
+        }
+        return willBe;
+    }
+
+}//end of class
